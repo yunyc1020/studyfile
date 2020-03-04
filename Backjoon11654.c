71c517bbf67764b7210a4443cc26f0b77077a471
yunyc1020 <49089088+yunyc1020@users.noreply.github.com>
2020-03-05 02:57:01 +0900

    Add files via upload

---
+++ Backjoon11654.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/*
+문제: 알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+입력: 알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+출력: 입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+*/
+int main(int argc, char *argv[]) {
+	char s;
+	scanf("%s", &s);
+	
+	printf("%d", s);
+	return 0;
+}
